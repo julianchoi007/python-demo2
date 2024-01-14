aecc17d94a83758c358ae15f1d65b728b0aa51f8
julianchoi007 <choijulian007@gmail.com>
2024-01-14 00:26:47 -0500

    playing around with tensors more

--- main.py
+++ main.py
@@ -8,7 +8,18 @@ import numpy as np
 # vector = torch.tensor([7, 7])
 # print(vector)
 
-Matrix = torch.tensor([[7,8],[9,10]])
-print(Matrix)
-print(Matrix[0])
-print(Matrix.shape)
+# Matrix = torch.tensor([[7,8],[9,10]])
+# print(Matrix)
+# print(Matrix[0])
+# print(Matrix.shape)
+
+# Random tensor
+# random_tensor = torch.rand(3, 4)
+
+# print(random_tensor)
+
+# random_image_size_tensor = torch.rand(size=(224, 224, 3)) # height, width, colour channel
+# print(random_image_size_tensor.shape, random_image_size_tensor.ndim)
+# demonstrates 3 colour channel image with size 224px by 224px
+
+# print(torch.rand(3, 3))
