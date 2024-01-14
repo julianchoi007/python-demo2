e24adf1db96a99e2cb9bcdc6c3288d8339b7dbef
julianchoi007 <choijulian007@gmail.com>
2024-01-13 23:58:44 -0500

    playing with tensors

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import torch
 import numpy as np
 
-x = torch.ones(5, requires_grad=True)
-y = torch.zeros(5)
-z = x + y
-print(x)
-print(y)
-print(z)
+# scalar = torch.tensor(7)
+# print(scalar.ndim)
+# print(scalar.item())
+
+# vector = torch.tensor([7, 7])
+# print(vector)
+
+Matrix = torch.tensor([[7,8],[9,10]])
+print(Matrix)
+print(Matrix[0])
+print(Matrix.shape)
